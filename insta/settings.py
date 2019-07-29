a46ea0c4c0c154f021f657abbede5d0185ddfb74
zubeir-Abubakar <zubkayare@gmail.com>
2019-07-29 11:59:21 +0300

    heroku deployment

--- insta/settings.py
+++ insta/settings.py
@@ -187,9 +187,9 @@ UPLOADCARE = {
     'secret':'8b3cceb6c375319683f6',
 }
 # Email configurations remember to install python-decouple
-EMAIL_USE_TLS = config('EMAIL_USE_TLS')
-EMAIL_HOST = config('EMAIL_HOST')
-EMAIL_PORT = config('EMAIL_PORT')
-EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+# EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+# EMAIL_HOST = config('EMAIL_HOST')
+# EMAIL_PORT = config('EMAIL_PORT')
+# EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 django_heroku.settings(locals())
