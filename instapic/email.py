b36b4797e3abfce4066a78562469f20710778725
zubeir-Abubakar <zubkayare@gmail.com>
2019-07-29 10:05:59 +0300

    creating text-contex and htm-content in the  email.py file

--- instapic/email.py
+++ instapic/email.py
@@ -5,11 +5,11 @@ from django.template.loader import render_to_string
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
     subject = 'Welcome to Instagram clone created by zubeyr Abubakar just for you!'
-    sender  = 'Zubkayare@gmail.com'
+    sender = 'Zubkayare@gmail.com'
 
     #passing in the context vairables
-    text_content = render_to_string('email/newsemail.txt',{"name": name})
-    html_content = render_to_string('email/newsemail.html',{"name": name})
+    text_content = render_to_string('email/instaemail.txt',{"name": name})
+    html_content = render_to_string('email/instaemail.html',{"name": name})
 
     msg = EmailMultiAlternatives(subject,text_content,sender,[receiver])
     msg.attach_alternative(html_content,'text/html')
