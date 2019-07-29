f2839e395f34b6dd458f284deaf56c0d18a94ab7
zubeir-Abubakar <zubkayare@gmail.com>
2019-07-29 10:05:13 +0300

    creating sender of the emailin the  email.py file

--- instapic/email.py
+++ instapic/email.py
@@ -4,8 +4,8 @@ from django.template.loader import render_to_string
 
 def send_welcome_email(name,receiver):
     # Creating message subject and sender
-    subje ct = 'Welcome to Instagram clone created by zubeyr Abubakar just for you!'
-    sender = 'Zubkayare@gmail.com'
+    subject = 'Welcome to Instagram clone created by zubeyr Abubakar just for you!'
+    sender  = 'Zubkayare@gmail.com'
 
     #passing in the context vairables
     text_content = render_to_string('email/newsemail.txt',{"name": name})
