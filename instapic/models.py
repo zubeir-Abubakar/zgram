a5d14aace757893012916588900e39d384b42c60
zubeir-Abubakar <zubkayare@gmail.com>
2019-07-29 10:22:52 +0300

    creating at class image  and defining save image function to save images for the owner in the models.py

--- instapic/models.py
+++ instapic/models.py
@@ -37,7 +37,7 @@ class Profile(models.Model):
 
 
 class Image(models.Model):
-    pic=ImageField(manual_crop='180x800', blank=True)
+    pic=ImageField(manual_crop='1080x800', blank=True)
     name= models.CharField(max_length=55)
     caption = models.TextField(blank=True)
     profile= models.ForeignKey(User, blank=True,on_delete=models.CASCADE)
@@ -48,7 +48,7 @@ class Image(models.Model):
     def __str__(self):
         return str(self.name)
 
-    def save_image(self):
+    def save_im age(self):
         self.save()
 
     def delete_image(self):
